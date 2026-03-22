787fb0a89c4cdd02d7f557a800b4ddbeba1aa6d8
Santosh Kumar <82412975+SantoshAnimesh@users.noreply.github.com>
2026-03-22 19:56:20 +0530

    Create App.js

---
+++ Machine-Coding/Redux-toolkit-setup/App.js
@@ -0,0 +1,11 @@
+import Display from "./Display";
+import "./styles.css";
+import UserAction from "./UserAction";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Display />
+    </div>
+  );
+}
